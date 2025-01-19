83e016fb33affe0b87808f6a0c40e06cf009000b
David Castelblanco Benavides <cbdavides@gmail.com>
2025-01-19 12:03:50 -0500

    Refactor commands definition

--- app/Main.hs
+++ app/Main.hs
@@ -8,22 +8,28 @@ import RandomNumberGenerator.Handler (processRNGCommand)
 
 import Options.Applicative
 
+data CLICommand a = CLICommand
+    { name        :: String
+    , parser      :: Parser a
+    , modifiers   :: InfoMod a
+    }
+
 data InputData = RandomNumber RNGInput | AESCipher AESCipherInput
     deriving (Show)
 
-
-parseInputData :: ParserInfo InputData
-parseInputData = info (commands  <**> helper)
-        (  fullDesc
-        <> progDesc "Cryptography utils")
-    where commands = subparser
-            (command "rng"
-                (info ((RandomNumber <$> parseNumberGeneratorInput) <**> helper)
-                (progDesc "Generate a random number"))
-            <> command "aes-cipher"
-                (info ((AESCipher <$> parseAESCipherInput) <**> helper)
-                (progDesc "Encrypt or decrypt using AES block cipher"))
-            )
+cliCommands :: [CLICommand InputData]
+cliCommands =
+    [ CLICommand
+        { name = "rng"
+        , parser = RandomNumber <$> parseNumberGeneratorInput
+        , modifiers = progDesc "Generate a random number"
+        }
+    , CLICommand
+        { name = "aes-cipher"
+        , parser = AESCipher <$> parseAESCipherInput
+        , modifiers = progDesc "Encrypt or decrypt using AES block cipher"
+        }
+    ]
 
 main :: IO ()
 main = process =<< customExecParser (prefs showHelpOnEmpty) parseInputData
@@ -31,3 +37,10 @@ main = process =<< customExecParser (prefs showHelpOnEmpty) parseInputData
 process :: InputData -> IO ()
 process (RandomNumber input) = processRNGCommand input
 process (AESCipher _) = putStrLn "Hey!"
+
+parseInputData :: ParserInfo InputData
+parseInputData = info (commands  <**> helper) (fullDesc <> progDesc "Cryptography utils")
+    where commands = subparser (mconcat $ buildCommand <$> cliCommands)
+
+buildCommand :: CLICommand a -> Mod CommandFields a
+buildCommand cmd = command (name cmd) (info (parser cmd <**> helper) (modifiers cmd))
